7003701674d5ed52397124d017c57730e1d16999
peyha <peyha.nicole@outlook.fr>
2024-02-23 09:54:58 +0100

    feat: display box

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod block_utils;
 use block_utils::{get_block, get_block_number};
 
 mod utils;
+use utils::{display_pretty_block, DisplayType};
 #[derive(Parser, Debug)]
 #[command(version, about)]
 struct Args{
@@ -20,11 +21,16 @@ async fn main() {
     
     let args = Args::parse();
 
-    let block_number = get_block_number(args.rpc.clone()).await.unwrap();
-    let lines = get_block(args.rpc, block_number).await.unwrap();
-
-    for line in lines {
-        println!("{}", line);
+    let mut cur_block_number = 0;
+    
+    loop {
+        
+        let block_number = get_block_number(args.rpc.clone()).await.unwrap();
+        if cur_block_number != block_number {
+               
+            let lines = get_block(args.rpc.clone(), block_number).await.unwrap();
+            display_pretty_block(lines, DisplayType::SingleLine);
+            cur_block_number = block_number;
+        }
     }
-    // print info of block
 }
