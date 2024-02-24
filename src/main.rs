23361a690e44ea28858cc042d26cb3f254020975
peyha <peyha.nicole@outlook.fr>
2024-02-24 13:22:23 +0100

    feat: handle main error

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use clap::Parser;
-
+use anyhow::Result;
 
 mod block_utils;
 use block_utils::{get_block, get_block_number};
 
 mod utils;
 use utils::{display_pretty_block, DisplayType};
+
 #[derive(Parser, Debug)]
 #[command(version, about)]
 struct Args{
@@ -27,10 +28,19 @@ async fn main() {
         
         let block_number = get_block_number(args.rpc.clone()).await.unwrap();
         if cur_block_number != block_number {
-               
-            let lines = get_block(args.rpc.clone(), block_number).await.unwrap();
-            display_pretty_block(lines, DisplayType::SingleLine);
-            cur_block_number = block_number;
+            
+            match get_block(args.rpc.clone(), block_number).await {
+                Ok(lines) => {
+                    if let Result::Err(e) = display_pretty_block(lines, DisplayType::SingleLine) {
+                        println!("Block display error on block {}: {}", block_number, e)
+                    };
+                    cur_block_number = block_number;
+                },
+                Err(e) => {
+                    println!("Block retrieval error on block {}: {}", block_number, e);
+                }
+            };
+
         }
     }
 }
